b18295e0eb25417be546842f076441c09fca908e
Maxim Gorbunkov <gorbunkov@haulmont.com>
2021-12-20 18:17:55 +0400

    Initial commit

---
+++ src/test/java/com/company/addonsdemo/JmixAddonsDemoR2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.addonsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmixAddonsDemoR2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
